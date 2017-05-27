284c091c4252579184dec2a330aa137b80e84607
Adeola Uthman <authman2@gmail.com>
2017-05-27 16:00:46 -0400

    Add Firebase to project

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// FIREBASE
+import * as firebase from 'firebase';
+var config = {
+    apiKey: "AIzaSyDK7Vc9FQHA8en946cabzxues36IpYeyYk",
+    authDomain: "iot-dormroom-9558c.firebaseapp.com",
+    databaseURL: "https://iot-dormroom-9558c.firebaseio.com",
+    projectId: "iot-dormroom-9558c",
+    storageBucket: "iot-dormroom-9558c.appspot.com",
+    messagingSenderId: "105656743007"
+  };
+  firebase.initializeApp(config);
+
 import App from './App';
 
 
