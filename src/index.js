ae44d8cd180343be06377c4b80a8921b59ed0a16
Adeola Uthman <authman2@gmail.com>
2017-07-24 23:34:32 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import App from './App';
 
 // FIREBASE
 import * as firebase from 'firebase';
-import sA from '../public/serviceAccount.json';
-// var config = {
-//     apiKey: "AIzaSyDK7Vc9FQHA8en946cabzxues36IpYeyYk",
-//     authDomain: "iot-dormroom-9558c.firebaseapp.com",
-//     databaseURL: "https://iot-dormroom-9558c.firebaseio.com",
-//     projectId: "iot-dormroom-9558c",
-//     storageBucket: "iot-dormroom-9558c.appspot.com",
-//     messagingSenderId: "105656743007"
-// };
-// firebase.initializeApp(config);
-firebase.initializeApp(sA);
+//import sA from '../public/serviceAccount.json';
+var config = {
+    apiKey: "AIzaSyDK7Vc9FQHA8en946cabzxues36IpYeyYk",
+    authDomain: "iot-dormroom-9558c.firebaseapp.com",
+    databaseURL: "https://iot-dormroom-9558c.firebaseio.com",
+    projectId: "iot-dormroom-9558c",
+    storageBucket: "iot-dormroom-9558c.appspot.com",
+    messagingSenderId: "105656743007"
+};
+firebase.initializeApp(config);
+//firebase.initializeApp(sA);
 
 ReactDOM.render( <App />,document.getElementById('root') );
